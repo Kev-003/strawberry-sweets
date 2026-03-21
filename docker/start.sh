0a24e555f24d4596eeb14f8fb61d03d834b319bb
Kevern Angeles <kevern920@gmail.com>
2026-03-21 21:16:10 +0800

    fix: SQLite DB permissions

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+# Ensure SQLite database exists and is writable
+touch /var/www/database/database.sqlite
+chown www-data:www-data /var/www/database/database.sqlite
+chmod 664 /var/www/database/database.sqlite
+
+# Ensure storage and cache are writable
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
+
 # Cache config for production
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Ensure SQLite database exists
-touch /var/www/database/database.sqlite
 
 # Run migrations
 php artisan migrate --force
